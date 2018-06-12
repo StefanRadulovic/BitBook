2706a0ee2433d702cd22b18a0bf18e10630215c0
BIT Sutdent <classroom@bgit.rs>
2018-06-12 15:19:44 +0200

    created page only

---
+++ bit-book/src/app/Log In/LogInPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+
+class LogInPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+
+        return (
+            <div className='container'>
+                <div >
+                    <h3>BitBook Login</h3>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default LogInPage
