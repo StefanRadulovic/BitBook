528bc8411ada174aab2d640e379da458dc058e4a
student <student@bgit.rs>
2018-06-12 12:27:41 +0200

    Add Profile Service.

--- bit-book/src/app/myProfile/MyProfilePage.js
+++ bit-book/src/app/myProfile/MyProfilePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import apiUrl from '../../shared/constants';
-import userService from '../../services/userService';
-// import { Link } from 'react-router-dom';
+import profileService from '../../services/profileService';
+import { Link } from 'react-router-dom';
+import { apiUrl } from '../../shared/constants';
 
 
 class MyProfilePage extends Component {
@@ -14,7 +14,7 @@ class MyProfilePage extends Component {
 
 
     componentDidMount() {
-        userService.getProfile()
+        profileService.getProfile()
             .then(profile => {
                 this.setState({
                     profile: profile
@@ -23,11 +23,28 @@ class MyProfilePage extends Component {
     }
 
 
+    profileImage = () => {
+        if (this.state.profile.avatarUrl === undefined) {
+            return (
+                <img className="profileImg" src="https://intellihr.com.au/wp-content/uploads/2017/06/avatar_placeholder_temporary.png" />
+            )
+        } else {
+            return (
+                <img className="profileImg" src={this.state.profile.avatarUrl} />
+            )
+        }
+    }
+
 
     render() {
         return (
             <div className="container">
-                <p>Lorem ipsum</p>
+                {this.profileImage()}
+                <h1>{this.state.profile.name}</h1>
+                <p><Link>Edit profile</Link></p>
+                <p>{this.state.profile.aboutShort}</p>
+                <button type="button" class="btn btn-light">{this.state.profile.postsCount} posts</button>
+                <button type="button" class="btn btn-light">{this.state.profile.commentsCount} comments</button>
             </div>
         )
     }
