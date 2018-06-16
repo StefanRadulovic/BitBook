531b448870f3dcfb3ca369977ca7583c49f546af
Stefan Radulovic <stefan.radulovic@yahoo.com>
2018-06-16 10:06:43 +0200

    ?

--- bit-book/src/services/singleFeedItemService.js
+++ bit-book/src/services/singleFeedItemService.js
@@ -54,6 +54,8 @@ class SingleFeedItemService {
             }
         }).then(response => response.json())
             .then(data => {
+               
+                
                 return data
             });
     }
