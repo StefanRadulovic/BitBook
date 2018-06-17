15527babdd00f8ff1ea1db742def219c14fdd33a
Stefan Radulovic <stefan.radulovic@yahoo.com>
2018-06-17 16:08:30 +0200

    infiniti scroll started

--- bit-book/src/app/feed/InfiniteScroll.js
+++ bit-book/src/app/feed/InfiniteScroll.js
@@ -1,22 +1,37 @@
 import React from 'react';
+<<<<<<< HEAD
 // import feedService from '../../services/feedService';
+=======
+import infiniteFeedService from '../../services/feedService';
+>>>>>>> infiniti scroll started
 import { LoadingScreen } from '../partials/LoadingScreen';
 import { FeedContent } from './FeedContent';
 import { FilterPosts } from './FilterPosts';
 import { CreateNewPost } from '../createNewPost/CreateNewPost';
+<<<<<<< HEAD
 import infiniteScrollFeedService from '../../services/infiniteScrollFeedService';
+=======
+import InfiniteScroll from 'react-infinite-scroller'
+>>>>>>> infiniti scroll started
 
 export default class Feed extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+<<<<<<< HEAD
             posts: null
         }
         this.scroll = React.createRef()
+=======
+            hasMoreItems: true,
+            posts: null
+        }
+>>>>>>> infiniti scroll started
     }
 
     loadPosts = () => {
 
+<<<<<<< HEAD
         infiniteScrollFeedService.getPosts(1).then(data => {
 
 
@@ -41,6 +56,32 @@ export default class Feed extends React.Component {
         return this.state.posts === null ? <LoadingScreen /> : (
             <div className="feed">
                 <FeedContent posts={this.state.posts} refreshFeed={this.loadPosts} />
+=======
+        infiniteFeedService.getPosts().then(data => {
+            if(data.length!==this.state.length){
+            this.setState({
+                posts: data
+            });
+        }else{
+            this.setState({
+                hasMoreItems: false
+            });
+        }
+        });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+    
+    render() {
+        return this.state.posts === null ? <LoadingScreen /> : (
+            <div className="feed">
+          
+                <FeedContent posts={this.state.posts} hasMore={this.state.hasMoreItems} reshFeed={this.loadPosts} />
+
+                
+>>>>>>> infiniti scroll started
                 <FilterPosts />
                 <CreateNewPost refreshFeed={this.loadPosts} />
             </div>
--- bit-book/src/services/infiniteScrollFeedService.js
+++ bit-book/src/services/infiniteScrollFeedService.js
@@ -2,19 +2,35 @@ import { apiUrl, apiKey, getOutHeader } from '../shared/constants';
 
 class InfiniteFeedService {
 
+<<<<<<< HEAD
     getPosts(pageNumber) {
         const sessionId = getOutHeader()
 
         return fetch(`${apiUrl}Posts?$top=5&$skip=${pageNumber}`, {
+=======
+    getPosts() {
+        const sessionId = getOutHeader()
+
+        return fetch(apiUrl + 'Posts/', {
+>>>>>>> infiniti scroll started
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'Key': apiKey,
+<<<<<<< HEAD
                 'SessionId': sessionId,
             }
         }).then(response => {
             return response.json()
         })
+=======
+                'SessionId': sessionId
+            }
+        }).then(response => {
+            console.log('res'+response);
+            
+           return response.json()})
+>>>>>>> infiniti scroll started
             .then(data => {
                 return data
             });
