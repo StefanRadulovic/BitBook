bdfb7c933bfa0e400ab1e3a4fed37c0bb5e84ab7
BIT Sutdent <classroom@bgit.rs>
2018-06-19 16:30:06 +0200

    ?

--- bit-book/src/app/feed/FeedContent.js
+++ bit-book/src/app/feed/FeedContent.js
@@ -7,10 +7,7 @@ import { VideoPost } from './VideoPost';
 export const FeedContent = (props) => {
 
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> scroll event does not work
+
         <div className="feed-content" onScroll={(event) => { console.log(event) }} >
             {props.posts.length === 0 ? <NothingInFeed /> :
                 (props.posts.map((post, i) => {
@@ -24,30 +21,6 @@ export const FeedContent = (props) => {
                         return <VideoPost post={post} refreshFeed={props.refreshFeed} key={i} />;
                     }
                 }))}
-<<<<<<< HEAD
-=======
-        <div className="feed-content">
-            <InfiniteScroll
-                pageStart={0}
-                loadMore={props.loadPosts}
-                hasMore={props.hasMoreItems}
-                loader={<div>Loading ...</div>}>
-                {props.posts.length === 0 ? <NothingInFeed /> :
-                    (props.posts.map((post, i) => {
-                        if (post.type === "text") {
-                            return <TextPost post={post} refreshFeed={props.refreshFeed} key={i} />;
-                        }
-                        if (post.type === "image") {
-                            return <ImagePost post={post} refreshFeed={props.refreshFeed} key={i} />;
-                        }
-                        if (post.type === "video") {
-                            return <VideoPost post={post} refreshFeed={props.refreshFeed} key={i} />;
-                        }
-                    }))}
-            </InfiniteScroll>
->>>>>>> infiniti scroll started
-=======
->>>>>>> scroll event does not work
         </div>
     );
 }
